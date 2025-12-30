4df8ca8ca46198e2450c53fde91102c6ad1c7ebc
atterpac <michael@getgalaxy.io>
2025-12-29 19:40:43 -0700

    fix verison with go install

--- internal/update/version.go
+++ internal/update/version.go
@@ -1,5 +1,10 @@
 package update
 
+import (
+	"runtime/debug"
+	"strings"
+)
+
 // Version information - injected at build time via ldflags:
 // go build -ldflags "-X github.com/atterpac/tempo/internal/update.Version=1.2.3 -X github.com/atterpac/tempo/internal/update.Commit=abc123 -X github.com/atterpac/tempo/internal/update.BuildDate=2024-01-01"
 var (
@@ -7,3 +12,34 @@ var (
 	Commit    = "unknown"
 	BuildDate = "unknown"
 )
+
+func init() {
+	// If version wasn't set via ldflags, try to get it from Go's build info
+	// This works when installed via: go install github.com/atterpac/tempo/cmd/tempo@latest
+	if Version == "dev" {
+		if info, ok := debug.ReadBuildInfo(); ok {
+			// Module version (e.g., "v0.0.6" or "(devel)")
+			if info.Main.Version != "" && info.Main.Version != "(devel)" {
+				Version = info.Main.Version
+			}
+
+			// Get VCS info for commit and time
+			for _, setting := range info.Settings {
+				switch setting.Key {
+				case "vcs.revision":
+					if len(setting.Value) >= 7 {
+						Commit = setting.Value[:7]
+					} else {
+						Commit = setting.Value
+					}
+				case "vcs.time":
+					BuildDate = setting.Value
+				case "vcs.modified":
+					if setting.Value == "true" && !strings.Contains(Version, "dirty") {
+						Version += "-dirty"
+					}
+				}
+			}
+		}
+	}
+}
